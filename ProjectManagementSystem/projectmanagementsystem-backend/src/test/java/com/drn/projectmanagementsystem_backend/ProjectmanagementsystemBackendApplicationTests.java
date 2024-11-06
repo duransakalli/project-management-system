6acc05e54716039e1ee97a807e263e5dcdf581a7
Duran S. <sakalli.duran@gmail.com>
2024-11-05 20:31:07 -0800

    created backend project

---
+++ ProjectManagementSystem/projectmanagementsystem-backend/src/test/java/com/drn/projectmanagementsystem_backend/ProjectmanagementsystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drn.projectmanagementsystem_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagementsystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
